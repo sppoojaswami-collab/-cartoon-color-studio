af5344172bff3426474725f114bc96d22e140be4
sppoojaswami-collab <sp.poojaswami@gmail.com>
2026-08-29 10:39:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,12 @@
-const V='cartoon-color-studio-fixed-v2';
-self.addEventListener('install',e=>e.waitUntil(caches.open(V).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const V='ccs-better-images-v1';
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET')return;
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+   if(e.request.url.includes('vectordad.com')){
+     const c=caches.open(V).then(x=>x.put(e.request,res.clone()));
+   }
+   return res;
+ }).catch(()=>caches.match(e.request))));
+});
